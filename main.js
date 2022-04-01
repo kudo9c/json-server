f39f1c45740b2e199aaf26649aff0872fa6bcf1f
kudo9c <kudo9c88@gmail.com>
2022-04-01 21:18:25 +0700

    change output

--- main.js
+++ main.js
@@ -27,22 +27,29 @@ server.use((req, res, next) => {
 })
 
 router.render = (req, res) => {
-  const headers = res.getHeaders()
-  const totalCountHeader = headers['x-total-count']
+  // Check GET with pagination
+  // If yes, custom output
+  const headers = res.getHeaders();
+
+  const totalCountHeader = headers['x-total-count'];
   if (req.method === 'GET' && totalCountHeader) {
-    const queryParams = queryString.parse(req._parsedUrl.query)
+    const queryParams = queryString.parse(req._parsedUrl.query);
+
     const result = {
       data: res.locals.data,
       pagination: {
         _page: Number.parseInt(queryParams._page) || 1,
         _limit: Number.parseInt(queryParams._limit) || 10,
-        _totalRows: Number.parseInt(totalCountHeader)
-      }
-    }
-  }
-  res.jsonp(res.locals.data)
-}
+        _totalRows: Number.parseInt(totalCountHeader),
+      },
+    };
 
+    return res.jsonp(result);
+  }
+
+  // Otherwise, keep default behavior
+  res.jsonp(res.locals.data);
+};
 // Use default router
 const PORT = process.env.PORT || 3000
 server.use("/api",router)
